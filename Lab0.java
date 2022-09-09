3c1a6125d3f1335a8712f3bf23813f14c330c352
YutaUWS <113057001+YutaUWS@users.noreply.github.com>
2022-09-09 11:52:08 -0500

    Create Lab0.java

---
+++ Lab0.java
@@ -0,0 +1,12 @@
+public class Lab0
+{
+    public static void main(String[] arg)
+    {
+        //Print Hello World! in a more complex way
+        String helloWorld = "Hello World!";
+        String notSentient = "Don't worry I'm not santient";
+        System.out.println(helloWorld + "\n" +  notSentient);
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
